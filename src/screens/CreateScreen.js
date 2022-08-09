1bd5def304200dc897776f128eb6087b60cbf5d9
Marcin Gąsowski <43890457+MGasowski@users.noreply.github.com>
2022-08-09 08:35:27 +0200

    Feat: creation screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,22 +1,58 @@
 import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
       <Text>index screen </Text>
       <Button title="add blog post" onPress={addBlogPost} />
       <FlatList
         data={state}
-        keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        keyExtractor={(blogPost) => blogPost.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Show", { id: item.id })}
+          >
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather name="trash" style={styles.icon} />
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    borderColor: "gray",
+    borderBottomWidth: 1,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
